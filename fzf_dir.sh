008db410922b8a8d726650f7b36e3e4d1090d655
Tiago Araujo <tiagomuniz130@gmail.com>
2023-11-05 20:28:00 -0300

    create new options and a readme for fzf_zellij

--- fzf_dir.sh
+++ fzf_dir.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/bash
 
+if ! [ -x $(command -v fzf) ]; then
+  echo "󱧖 -> fzf not found (run: yay -S fzf)" | lolcat
+  exit
+fi
+
+if ! [ -x $(command -v zellij) ]; then
+  echo "󱧖 -> zellij not found (run: yay -S zellij)" | lolcat
+  exit
+fi
+
+if [ -x $(command -v lolcat) ]; then
+  echo "󱧖 -> zellij not found (run: yay -S lolcat)" | lolcat
+  exit
+fi
+
 EXCLUDE_DIRS=(
   ".*"
   "node_modules"
@@ -17,25 +32,46 @@ EXCLUDE_DIRS=(
   "HyprV4"
 )
 
-FIND_COMMAND="find $HOME -type d"
-
-for dir in "${EXCLUDE_DIRS[@]}"; do
-  FIND_COMMAND+=" -name '$dir' -prune -o"
-done
-
-FIND_COMMAND+=" -type d -print"
-
-SELECTED_PATH=$(eval "$FIND_COMMAND" | fzf)
-
-if [ $SELECTED_PATH ] && [ $2 ]; then
-  cd $SELECTED_PATH
-  if [ $1 == "-t" ]; then
-    zellij action new-tab -n $2 --layout ~/.config/zellij/zdir.kdl
-  elif [ $1 == "-s" ]; then
-    zellij --session $2
-  else
+if [ $# == 0 ]; then
+  if [ $(zellij ls | wc -l) == 0 ]; then
     zellij --session default
+  else
+    session=$(zellij ls | fzf --prompt="󱊄 Select a session: " --height=15% --color --pointer=" " --min-height=5 --layout=reverse --border)
+    if [[ $session ]]; then
+      zellij a $session
+    else
+      echo "󰂭  No session selected!"
+    fi
   fi
+elif [ $# == 1 ]; then
+  echo "󰂭  invalid command: fzj $1 [value]"
 else
-  echo "❌ No path selected!"
+  FIND_COMMAND="find $HOME -type d"
+
+  for dir in "${EXCLUDE_DIRS[@]}"; do
+    FIND_COMMAND+=" -name '$dir' -prune -o"
+  done
+
+  FIND_COMMAND+=" -type d -print"
+
+  SELECTED_PATH=$(eval "$FIND_COMMAND" | fzf --prompt="󰥨 Search dir: " --height=40% --min-height=5 --pointer=" " --layout=reverse --border --preview "ls -la --color {}")
+
+  if [ $SELECTED_PATH ]; then
+    cd $SELECTED_PATH
+    
+    if [ $1 == "-t" ]; then
+      zellij action new-tab -n $2
+    elif [ $1 == "-s" ]; then
+      zellij --session $2
+    elif [ $1 == "-f"]; then
+      #  TODO: Select a file with fzf to edit on a pane in zellij
+      
+      # zellij action edit $file
+      echo "todo"
+    else
+      echo "something" | lolcat
+    fi
+  else
+    echo "󰂭  No path selected!" | lolcat
+  fi
 fi
